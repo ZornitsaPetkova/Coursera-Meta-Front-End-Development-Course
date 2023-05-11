6832660b51d9d21e03dd0b01fa30645a2d32dad9
Zornitsa Petkova <131281700+ZornitsaPetkova@users.noreply.github.com>
2023-05-11 10:41:09 +0300

    Add files via upload

---
+++ Week 5/03.test.js
@@ -0,0 +1,50 @@
+
+/* const a = true;
+if(!a) {
+    console.log("Green");
+} else {
+    console.log("Blue");
+}
+
+
+
+var message = "Hello";
+message += " World!";
+message = "Goodbye!";
+console.log(message);
+
+
+
+
+var x = 20;
+if(x >= 10) {
+    console.log("Apple");
+} else if(x <= 5) {
+    console.log("Pear");
+} else {
+    console.log("Orange");
+}
+
+
+var result = 0;
+
+var i = 0;
+var limit = 3;
+while(i < limit) {
+    result += 2;
+    i++;
+}
+
+console.log(result);
+*/
+
+
+
+var cat = {}
+cat.sound = "meow"
+var catSound = "purr"
+console.log(cat.sound)
+
+
+var veggies = ['parsley', 'carrot']
+console.log(veggies[2])
